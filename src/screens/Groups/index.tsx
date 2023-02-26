e514390d930396699e17b3b18e86989e27be3065
Anderson Silva <anderson.consultant.it@outlook.com>
2023-02-26 16:03:20 -0300

    feat: added styled components to Group component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,10 @@
-import { Text, View } from 'react-native';
-import { styles } from './styles';
+import { Container, Title } from './styles';
 
 
 export function Groups() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Groups</Text>
-    </View>
+    <Container>
+      <Title>Groups</Title>
+    </Container>
   );
 }
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,10 +1,13 @@
-import { StyleSheet } from 'react-native';
+import styled from 'styled-components/native';
 
-export const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-  },
-  title: {
-    fontSize: 24,
-  }
-})
+export const Container = styled.View`
+  flex: 1;
+  background-color: #000;
+  align-items: 'center';
+  justify-content: 'center';
+`;
+
+export const Title = styled.Text`
+  font-size: 32;
+  color: #fff;
+`;
