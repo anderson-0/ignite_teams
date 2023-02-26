e022c0cd18a2d9165c3509f48952d968e18ec6e7
Anderson Silva <anderson.consultant.it@outlook.com>
2023-02-26 19:00:06 -0300

    feat: added expo google fonts roboto and activity indicator

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
+import { ActivityIndicator } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { Groups } from './src/screens/Groups';
 import theme from './src/theme';
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
   return (
     <ThemeProvider theme={theme}>
-    <Groups/>
+      {fontsLoaded ? <Groups/> : <ActivityIndicator/>}
     </ThemeProvider>
   );
 }
