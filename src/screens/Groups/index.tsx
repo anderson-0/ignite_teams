d4be4b94a6350d37e74e4ca9ed115b0704ab7069
Anderson Silva <anderson.consultant.it@outlook.com>
2023-02-26 10:00:59 -0300

    first: Groups component basic structure

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+import { styles } from './styles';
+
+
+export function Groups() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Groups</Text>
+    </View>
+  );
+}
