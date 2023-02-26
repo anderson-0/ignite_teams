29dc4e1df94995f947f78eae6f06283265b94d62
Anderson Silva <anderson.consultant.it@outlook.com>
2023-02-26 19:45:03 -0300

    feat: added Header component to Groups screen

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,11 @@
+import { Header } from '@components/Header';
 import { Container, Title } from './styles';
 
 
 export function Groups() {
   return (
     <Container>
+      <Header />
       <Title>Groups</Title>
     </Container>
   );
