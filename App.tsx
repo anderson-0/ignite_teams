7aac1559e943f66d904c25baa03c44629b043d60
Anderson Silva <anderson.consultant.it@outlook.com>
2023-02-26 16:03:28 -0300

    feat: added theme to project

--- App.tsx
+++ App.tsx
@@ -1,7 +1,10 @@
+import { ThemeProvider } from 'styled-components';
 import { Groups } from './src/screens/Groups';
-
+import theme from './src/theme';
 export default function App() {
   return (
+    <ThemeProvider theme={theme}>
     <Groups/>
+    </ThemeProvider>
   );
 }
