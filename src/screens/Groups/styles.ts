ab98a09cfdcc0f7f97c482c360413f7af136485d
Anderson Silva <anderson.consultant.it@outlook.com>
2023-02-26 18:59:48 -0300

    feat: Groups component styles using theme values

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,12 +2,12 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  background-color: #000;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_700};
   align-items: 'center';
   justify-content: 'center';
 `;
 
 export const Title = styled.Text`
-  font-size: 32;
-  color: #fff;
+  font-size: 32px;
+  color: ${({ theme }) => theme.COLORS.WHITE};
 `;
