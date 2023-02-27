98950e86d7040770aae7d0ef53bb8d5055d28054
Anderson Silva <anderson.consultant.it@outlook.com>
2023-02-26 23:18:52 -0300

    feat: load groups dinamically from state

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,13 +1,26 @@
+import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 import { Container } from './styles';
 
 
 export function Groups() {
+  const [groups, setGroups] = useState([]);
   return (
     <Container>
       <Header showBackButton />
-      <Highlight title="Highlight" subtitle="Subtitle" />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard title={item} />
+        )}
+        ListEmptyComponent={() => (
+          <Highlight title="Nenhum grupo encontrado" subtitle="Crie um grupo para começar" />
+        )}
+      />
     </Container>
   );
 }
