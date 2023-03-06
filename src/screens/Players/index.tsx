5cd8f256a91488b4dbf0b690ba99dc07b0dc2ee6
Anderson Silva <60013836+anderson-0@users.noreply.github.com>
2023-03-06 10:57:19 -0300

    feat: add input and buttonIcon to Players screen

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,12 +1,18 @@
+import { ButtonIcon } from '@components/ButtonIcon';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
-import { Container } from './styles';
+import { Input } from '@components/Input';
+import { Container, Form } from './styles';
 
 export function Players() {
   return (
     <Container>
       <Header showBackButton />
       <Highlight title='Nome da Turma' subtitle='adicione a galera e separe os times' />
+      <Form>
+        <Input placeholder='Nome da pessoa' autoCorrect={false} />
+        <ButtonIcon icon='add' type='PRIMARY'/>
+      </Form>
     </Container>
   );
 }
