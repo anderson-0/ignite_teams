d5c5262a082f1bbc8b6e9421252da83762d834c7
Anderson Silva <60013836+anderson-0@users.noreply.github.com>
2023-03-05 21:51:00 -0300

    feat: Input component using theme

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,11 +1,16 @@
 import { TextInputProps } from 'react-native';
+import { useTheme } from 'styled-components/native';
 import { Container } from './styles';
 
 
 type Props = TextInputProps;
 
 export function Input({ ...rest }: Props) {
+
+  const { COLORS } = useTheme();
   return (
-    <Container {...rest} />
+    <Container 
+      placeholderTextColor={COLORS.GRAY_300}
+    {...rest} />
   );
 }
